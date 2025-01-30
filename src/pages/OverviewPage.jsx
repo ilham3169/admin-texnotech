0b981a6e64642f8abd56b20b80f59ef53c41ee74
ilham3169 <ilham.yaqubzada.std@bhos.edu.az>
2025-01-31 00:39:03 +0400

    overviev - product num

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -6,8 +6,27 @@ import StatCard from "../components/common/StatCard";
 import SalesOverviewChart from "../components/overview/SalesOverviewChart";
 import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
 import SalesChannelChart from "../components/overview/SalesChannelChart";
+import React, { useState, useEffect } from "react";
+
+
+
 
 const OverviewPage = () => {
+	const [allProducts, setAllProducts] = useState([]); 
+
+	useEffect(() => {
+		fetch("https://back-texnotech.onrender.com/products")
+			.then((response) => response.json())
+			.then((data) => {
+			const products = data.map((product) => ({
+				name: product.name,
+				id: product.id,
+			}));
+			setAllProducts(products);
+			})
+			.catch((error) => console.error("Error fetching products:", error));
+	}, []);
+
 	return (
 		<div className='flex-1 overflow-auto relative z-10'>
 			<Header title='Overview' />
@@ -21,12 +40,11 @@ const OverviewPage = () => {
 					transition={{ duration: 1 }}
 				>
 					<StatCard name='Total Sales' icon={Zap} value='$12,345' color='#6366F1' />
-					<StatCard name='New Users' icon={Users} value='1,234' color='#8B5CF6' />
-					<StatCard name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
-					<StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
+					{/* <StatCard name='New Users' icon={Users} value='1,234' color='#8B5CF6' /> */}
+					<StatCard name='Total Products' icon={ShoppingBag} value={allProducts.length} color='#EC4899' />
+					{/* <StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' /> */}
 				</motion.div>
 
-				{/* CHARTS */}
 
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
 					<SalesOverviewChart />
