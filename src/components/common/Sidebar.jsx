38d54375f9833836342a8ced3537f59985535b7e
refleks1a <fhbxdfh.dhncdtb@gmail.com>
2025-02-13 18:59:02 +0100

    sidebar: change redirect link

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -10,7 +10,7 @@ const SIDEBAR_ITEMS = [
 	// 	color: "#6366f1",
 	// 	href: "/",
 	// },
-	{ name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/products" },
+	{ name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/" },
 	// { name: "Users", icon: Users, color: "#EC4899", href: "/users" },
 	// { name: "Sales", icon: DollarSign, color: "#10B981", href: "/sales" },
 	// { name: "Orders", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
