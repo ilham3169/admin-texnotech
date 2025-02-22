19fab460e505e02f1bed43f0cf0f009a05f98f3f
refleks1a <fhbxdfh.dhncdtb@gmail.com>
2025-02-22 11:24:18 +0100

    sidebar: translate the sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -10,10 +10,10 @@ const SIDEBAR_ITEMS = [
 	// 	color: "#6366f1",
 	// 	href: "/",
 	// },
-	{ name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/" },
+	{ name: "Məhsullar", icon: ShoppingBag, color: "#8B5CF6", href: "/" },
 	// { name: "Users", icon: Users, color: "#EC4899", href: "/users" },
 	// { name: "Sales", icon: DollarSign, color: "#10B981", href: "/sales" },
-	{ name: "Orders", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
+	{ name: "Sifarişlər", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
 	// { name: "Analytics", icon: TrendingUp, color: "#3B82F6", href: "/analytics" },
 	// { name: "Settings", icon: Settings, color: "#6EE7B7", href: "/settings" },
 ];
